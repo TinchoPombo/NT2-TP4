f6c1bd19594a48bfd6583df095559182544cd210
TinchoPombo <martinpombo21@gmail.com>
2022-05-23 20:43:08 -0300

    ultimo ajuste

--- src/router.js
+++ src/router.js
@@ -9,6 +9,7 @@ Vue.use(VueRouter)
 export const router = new VueRouter({
     mode: 'history',
     routes: [
+        { path: '/', redirect: '/formulario' },
         {path: '/formulario', component: Formulario},
         {path: '/punto2', component: Punto2}
     ]
